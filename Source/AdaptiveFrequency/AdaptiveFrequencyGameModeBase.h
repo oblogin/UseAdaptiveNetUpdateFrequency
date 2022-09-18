54c0b0ed8f72ffbc17721d08c87135aa79283a90
i.oblogin <i.oblogin@game-positive.com>
2022-09-18 17:37:40 +0300

    Initial Commit

---
+++ Source/AdaptiveFrequency/AdaptiveFrequencyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AdaptiveFrequencyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ADAPTIVEFREQUENCY_API AAdaptiveFrequencyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
